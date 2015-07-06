08cc4bf614e347d9d189d6cf930462ed7428bf63
rofrischmann <robin@rofrischmann.de>
2015-07-06 20:59:31 +0200

    removed old demo tests

--- test/performance/cases.js
+++
@@ -1,9 +0,0 @@
-let addCSSRule = new Set();
-addCSSRule.add('color: blue');
-//addCSSRule.add('color: blue; background-color: red');
-addCSSRule.add('color: blue; background-color: red; font-size: 15px');
-//addCSSRule.add('color: blue; background-color: red; font-size: 15px; transition: 300ms all linear');
-
-export default {
-	addCSSRule: addCSSRule
-}
